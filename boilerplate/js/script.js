5b07dc907d734cb3246b79ea826393b1609e8bc7
Dade007 <42833478+Dade007@users.noreply.github.com>
2018-09-01 19:16:33 +0400

    apple task

--- boilerplate/js/script.js
+++ boilerplate/js/script.js
@@ -1,1 +1,14 @@
-console.log('You are at' + window.location);
+d = prompt('Сколько у вас денег?')
+d = +d
+ya = prompt('Сколько вы купили яблок?')
+ya = +ya
+b = prompt('Сколько вы купили батонов?')
+b = +b
+
+sya = prompt('Сколько стоит 1 яблоко?')
+sya = +sya
+sb = prompt('Сколько стоит 1 батон ?')
+sb= +sb
+
+r = d > ya*sya + b*sb
+document.body.innerHTML = r
