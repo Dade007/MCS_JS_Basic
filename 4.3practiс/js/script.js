0e5c9a806cb03fc2bcd82ab239ae9db3355b6a94
Dade007 <42833478+Dade007@users.noreply.github.com>
2018-09-04 21:19:09 +0400

    4.3practic

---
+++ 4.3practiс/js/script.js
@@ -0,0 +1,19 @@
+ //const M = document.querySelector('.modul');
+
+
+ let M = prompt('Введите число');
+ M = parseInt(M);
+
+(M >= 0) ? console.log(M) :  console.log(-M);
+
+ //if (M>0){
+ //    console.log(M);   
+ //} else if (M<0) {
+ //	M = -M;
+ //	console.log(M); 
+ 
+ //} else {
+// 	M = 0;
+ //	console.log(M);
+ //}
+
